d54ab59d7ea23d1cdb69b524c3ad8647d9a8d286
Molnija3d <molnija3d@yandex.ru>
2025-10-24 23:14:00 +0300

    add comments in main.c

--- HW_03-Assembler/main.c
+++ HW_03-Assembler/main.c
@@ -31,13 +31,14 @@ int main(void)
     filtered = f(list, NULL, p);
     m(filtered, print_int);
     puts("");
-    
+
     free_list(list);
     free_list(filtered);
 
     return 0;
 }
-
+/* Filter function, it use a pointer to predicate function to filter the list recursively.
+Return value - pointer to filtered list. */
 list_t *f(list_t *list, list_t *store, int (*func)(long int))
 {
 
@@ -52,6 +53,9 @@ list_t *f(list_t *list, list_t *store, int (*func)(long int))
     return store;
 }
 
+/*
+Mapping function. It apply specified function (second parameter) to every element of the list recursively.
+*/
 void m(list_t *list, void (*func)(long int))
 {
     if (list)
@@ -61,11 +65,17 @@ void m(list_t *list, void (*func)(long int))
     }
 }
 
+/*
+Print a value to terminal
+*/
 void print_int(long int value)
 {
     printf("%ld ", value);
 }
 
+/*
+Adds new value to the list. Return value - pointer to the new head of the list.
+*/
 list_t *add_element(list_t *next, long int data)
 {
     list_t *node = malloc(sizeof(list_t));
@@ -81,15 +91,22 @@ list_t *add_element(list_t *next, long int data)
     return node;
 }
 
-int p(long int a) 
+/*
+Predicate function. Parity check of the parameter.
+return value - 0 for even, 1 for odd numbers.
+*/
+int p(long int a)
 {
     return a & 0x1;
 }
 
+/*
+Recursively free list
+*/
 void free_list(list_t *list)
 {
     if (list)
-    { 
+    {
         free_list(list->next);
         free(list);
     }
