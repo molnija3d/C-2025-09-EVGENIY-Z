664d2a66f4d35aca4f85b9cab23051149cfbd920
Molnija3D <molnija3d@yandex.ru>
2026-03-11 21:13:31 +0300

    refactor valgrind script

--- FINAL/test/valgrind.sh
+++ FINAL/test/valgrind.sh
@@ -1,1 +1,1 @@
-valgrind --leak-check=full --track-origins=yes ./torrent_client -f Chinese_Ghost_Story.torrent
+valgrind --leak-check=full --track-origins=yes ./torrent_client -f Chinese_Ghost_Story.torrent -O downloaded 
